1ae104a1778c603af01fd5b106b7bc15d7efac0b
Josh Morrow <jcmorrow@thoughtbot.com>
2016-06-17 12:46:40 -0700

    Linter::Coffeescript uses merged config
     - Merge the owner's coffeescript config (defaults to default
       coffeescript config) with the repo level overrides.
     - Change documentation to reflect owner-level config

--- app/models/config/coffee_script.rb
+++ app/models/config/coffee_script.rb
@@ -1,9 +1,13 @@
 module Config
   class CoffeeScript < Base
+    def merge(config)
+      content.deep_merge(config.content)
+    end
+
     private
 
     def parse(file_content)
-      Parser.json(file_content)
+      Config::Parser.json(file_content)
     end
   end
 end
